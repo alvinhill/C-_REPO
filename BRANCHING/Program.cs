3d1b4af78732e3ab66f350295bfd2767a5eef0f5
alvinhill <rainfirex@att.net>
2023-07-16 11:46:41 -0700

    NEW COMMIT

---
+++ BRANCHING/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRANCHING
+{
+    class Program
+    {
+        static void Main()
+        {
+            int currtemp = 70;
+            int roomtemp = 70;
+
+            if (currtemp == roomtemp)
+            {
+                Console.Write("they are the same");
+            }
+
+
+
+
+
+
+        }
+    }
+}
