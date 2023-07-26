347dbd7e0679449b6a804c77927a75437ebf309e
alvinhill <rainfirex@att.net>
2023-07-25 18:00:42 -0700

    STEP 275

---
+++ CALLING METHODS SUBMISSION ASSIGNMENT/CALC.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
+{
+    class CALC
+    {
+
+        // DEFINE VARIABLES
+        public int numU;
+        public int numMU = 10;
+        public int numC = 100;
+        public int numSC;
+        public int numMC;
+        public int numSUS = 10;
+
+        // ADDITION SECTION
+        public int Addnum()
+        {
+            return numU + numC;
+        }
+
+        // SUBTRACTION SECTION
+        public int Subnum()
+        {
+            return numSC - numSUS;
+        }
+
+        // MULTIPLICATION SECTION
+        public int Multnum()
+        {
+            return numMU * numMC;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CALLING METHODS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CREATE OBJECT
+            CALC calc = new CALC();
+
+
+            Console.Write("Add 100 to Your Number: ");
+            calc.numU = Convert.ToInt32(Console.ReadLine());
+
+            // OUTPUT ADDITION ANSWER
+            Console.WriteLine(calc.Addnum());
+            Console.ReadLine();
+
+            Console.Write("Subtract 10 From Your Number: ");
+            calc.numSC = Convert.ToInt32(Console.ReadLine());
+
+            // OUTPUT SUBTRACTION ANSWER
+            Console.WriteLine(calc.Subnum());
+            Console.ReadLine();
+
+
+            Console.Write("Multply Your Number BY 10: ");
+            calc.numMC = Convert.ToInt32(Console.ReadLine());
+
+            // OUTPUT MULTIPLICATION ANSWER
+            Console.WriteLine(calc.Multnum());
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
