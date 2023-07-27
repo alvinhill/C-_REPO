b472e96ca63b4eef08678c971fd21bdd67383465
alvinhill <rainfirex@att.net>
2023-07-26 21:03:49 -0700

    COMMIT 279

---
+++ MAIN SUBMISSION ASSIGNMENT SREP 279/CALC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_SUBMISSION_ASSIGNMENT_SREP_279
+{
+    class CALC
+    {
+
+        // DECLARE VARIABLES
+        public int numOne;
+        public decimal numTwo;
+        public string numThree;
+
+
+        // CREATE INT CLASS
+        public int DoMath(int numOne)
+        {
+            int result = numOne * 5;
+            return result;
+        }
+
+        // CREATE DECIMAL CLASS OVERLOAD
+        public decimal DoMath(decimal numTwo)
+        {
+            decimal result = numTwo - 15.27m;
+            return result;
+        }
+
+
+        // CREATE STRING CLASS OVERLOAD
+        public string DoMath(string numThree)
+        {
+            int result = Convert.ToInt32(numThree) + 10;
+            return Convert.ToString(result);
+        }
+
+
+    }
+}
---
+++ MAIN SUBMISSION ASSIGNMENT SREP 279/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_SUBMISSION_ASSIGNMENT_SREP_279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // CREATE OBJECT
+            CALC calc = new CALC();
+
+            // ENTER NUMBER INTO CALC CLASS
+            Console.Write("Enter Int Number to Multify by Five ");
+            calc.numOne = Convert.ToInt32(Console.ReadLine());
+
+            // GET MATH OPERATION FROM FIRST CLASS
+            Console.WriteLine(calc.DoMath(calc.numOne));
+            Console.ReadLine();
+
+            // ENTER NUMBER INTO CALC CLASS
+            Console.Write("Enter Decimal Number That Will Be Subtracted By 15.27 ");
+            calc.numTwo = Convert.ToDecimal(Console.ReadLine());
+
+            // GET MATH OPERATION FROM NEXT OVERLOADED CLASS
+            Console.WriteLine(calc.DoMath(calc.numTwo));
+            Console.ReadLine();
+
+            // ENTER NUMBER INTO CALC CLASS
+            Console.Write("Enter A Number to Add 10 To ");
+            calc.numThree = (Console.ReadLine());
+
+            // GET MATH OPERATION FROM NEXT OVERLOADED CLASS
+            Console.WriteLine(calc.DoMath(calc.numThree));
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
