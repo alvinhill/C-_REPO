351092e6c681a31fc61ec48ebe182bc0a7d6b88b
alvinhill <rainfirex@att.net>
2023-07-29 17:09:32 -0700

    FINAL OUTPUT NOT WORKING

---
+++ METHOD CLASS SUBMISSION ASSIGNMENT 281/MAKE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT_281
+{
+    class MAKE
+    {
+        public void doAction(int num1, int num2)
+
+        {
+            num1 = num1 + 5;
+            Console.WriteLine("print int num2 from MAKE CLASS: "+num2);
+            Console.ReadLine();
+        }
+              
+    }
+   
+
+}
---
+++ METHOD CLASS SUBMISSION ASSIGNMENT 281/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT_281
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            // CREATE OBJECT
+            MAKE make = new MAKE();
+
+            // PLACE VALUES IN THE MAKE METHOD
+            make.doAction(72, 43);
+
+            // PASS VALUES INTO THE MAKE METHOD BY NAME
+            make.doAction(num1: 17 , num2: 11);
+            Console.ReadLine();
+
+            // FINAL OUTPUT NOT WORKING
+            //Console.WriteLine(make.doAction(num1:,num2:));
+
+            Console.ReadLine();
+
+        }
+    }
+}
