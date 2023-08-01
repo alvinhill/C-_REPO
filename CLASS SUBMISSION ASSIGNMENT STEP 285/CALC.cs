6c7aa88be6715244fca3326211cafb42b698173e
alvinhill <rainfirex@att.net>
2023-07-31 18:09:19 -0700

    COMMIT 285

---
+++ CLASS SUBMISSION ASSIGNMENT STEP 285/CALC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT_STEP_285
+{
+    class CALC
+    {
+        // METHOD WITH OUT
+        public void DoWork(int numOne, out int answer)
+        {
+            answer = numOne / 2;
+
+        }
+        // METHOD WITH TWO OUTS
+        public void Data(int a, int b, out int x, out int y)
+        {
+            x = a + b;
+            y = a * b;
+        }
+
+        // OVERLOAD METHOD WITH OUTS
+        public void Data(int c, int d, int e, out int z)
+        {
+            z = c + d + e;
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ CLASS SUBMISSION ASSIGNMENT STEP 285/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT_STEP_285
+{
+    // PART SIX
+    //STATIC CLASS
+    static class data
+    {
+        public static string FName;
+
+        // STATIC METHOD
+        public static void GName()
+        {
+            Console.WriteLine("ENTER NAME ");
+            FName = Console.ReadLine();
+            Console.WriteLine("name is " + FName);
+            Console.ReadLine();
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CLASS SUBMISSION ASSIGNMENT STEP 285
+
+            // CREATE OBJECT
+            CALC calc = new CALC();
+
+            //part 1 and 2 
+            // ENTER NUMBER INTO THE DoWork METHOD IN THE CALC CLASS
+            Console.WriteLine("PART 1 and 2 create void class that outputs integer");
+            Console.Write("Divide your number by 2");
+            Console.WriteLine("");
+
+            // numOne IS THE VALUE SENT
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            //part 3 
+            // GET THE RESULT OF DoWork METHOD
+            Console.WriteLine("PART 3 call method to screen");
+            Console.WriteLine("");
+            calc.DoWork(numOne, out int answer);
+            // DISPLAY THE RESULT
+            Console.Write("THE ANSWER = " + answer);
+            Console.ReadLine();
+
+            // part 4
+            Console.WriteLine("PART 4 output parameters");
+            Console.WriteLine("");
+            Console.Write("ENTER FIRST NUMBER a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ENTER SECOND NUMBER b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            calc.Data(a, b, out int x, out int y);
+            // DISPLAY THE RESULT
+            Console.WriteLine("THE ANSWER- a plus b = " + x + "   a times b = " + y);
+            Console.ReadLine();
+
+            // part 5
+            Console.WriteLine("PART 5 overload method ");
+            Console.WriteLine("");
+            Console.Write("ENTER FIRST NUMBER c ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ENTER SECOND NUMBER d ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ENTER THIRD NUMBER e ");
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            // DO MATH
+            calc.Data(c, d, e, out int z);
+            // DISPLAY THE RESULT
+            Console.WriteLine("THE ANSWER- c plus d plus e = " + z);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
