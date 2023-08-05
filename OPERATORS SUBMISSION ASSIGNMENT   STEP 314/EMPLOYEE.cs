1464b079d51da83e9d07f850b88b527427319c70
alvinhill <rainfirex@att.net>
2023-08-04 18:16:31 -0700

    COMMIT STEP 314

---
+++ OPERATORS SUBMISSION ASSIGNMENT   STEP 314/EMPLOYEE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION_ASSIGNMENT___STEP_314
+{
+    public class EMPLOYEE
+    {
+        // CREATE 3 PROPERITES 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        // OVERLOAD == OPERATOR
+        public static bool operator ==(EMPLOYEE employee1, EMPLOYEE employee2)
+        {
+            if (employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        // OVERLOAD  != OPERATOR
+        public static bool operator !=(EMPLOYEE employee1, EMPLOYEE employee2)
+        {
+            if (!employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        // OVERRIDE EQUALS MEHTOD
+        public override bool Equals(object obj)
+        {
+            EMPLOYEE employee = obj as EMPLOYEE;
+
+        if (employee !=null)
+
+        {
+            return employee.Id.Equals(this.Id);
+        }
+            return false;
+        }
+
+        // OVERRIDE HASH CODE METHOD
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
+
+    }
+   
+}
---
+++ OPERATORS SUBMISSION ASSIGNMENT   STEP 314/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION_ASSIGNMENT___STEP_314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CREATE TWO OBJECTS WITH DIFFERENT PROPERTY VALUES
+            EMPLOYEE employee1 = new EMPLOYEE();
+            employee1.Id = 1;
+            employee1.FirstName = "Fred";
+            employee1.LastName = "Davis";
+
+            EMPLOYEE employee2 = new EMPLOYEE();
+            employee2.Id = 2;
+            employee2.FirstName = "June";
+            employee2.LastName = "Travis";
+
+            // COMPARE OBJECTS WITH ==
+            if (employee1==employee2)
+            {
+                Console.WriteLine("These employees are equal ");
+            }
+            else
+            {
+                Console.WriteLine("These employees are NOT equal ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
