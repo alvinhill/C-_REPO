9efe3420b585eb6be7e7ac4dd9ca57618eb6a319
alvinhill <rainfirex@att.net>
2023-08-08 22:53:23 -0700

    PUSH 2

--- BLACKJACK/Program.cs
+++ BLACKJACK/Program.cs
@@ -10,6 +10,32 @@ namespace BLACKJACK
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcom to the Casino");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("How Much Mondey Do You Have");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello,{0},Would you like to join a game of 21 right now? ", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "Y")
+            {
+                PLAYER player = new PLAYER(playerName, bank);
+                GAME game = new TWENTYONEGAME();
+                game += player;
+                player.isActivlyPlaying = true;
+                while (player.isActivlyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game += player;
+                Console.WriteLine("thank you for playing");
+            }
+            Console.WriteLine("Feel free to look aroung the casino. bye for now. ");
+            Console.Read();
+
+
+
         }
     }
 }
